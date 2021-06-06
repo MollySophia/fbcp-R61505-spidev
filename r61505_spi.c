66cda7bd705f8fd203d0ebbb08bc4f7bb5cfe583
Molly Sophia <mollysophia379@gmail.com>
2021-06-06 20:48:31 +0800

    Update r61505_spi.c

--- r61505_spi.c
+++ r61505_spi.c
@@ -202,8 +202,7 @@ void lcd_drawBlock(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint
             pos = 0;
         }
     }
-    if(pos != 0)
-        spi_write(pos, data);
+    spi_write(pos, data);
     spi_cs(1);
 }
 
